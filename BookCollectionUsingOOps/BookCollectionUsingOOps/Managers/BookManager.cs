0292d49628613ad89b438ca46a214557cd285a79
majji82 <2100032153cseh@gmail.com>
2025-01-19 14:41:39 +0530

    completed project

---
+++ BookCollectionUsingOOps/BookCollectionUsingOOps/Managers/BookManager.cs
@@ -0,0 +1,110 @@
+﻿using BookCollectionUsingOOps.Data;
+using BookCollectionUsingOOps.Models;
+using BookCollectionUsingOOps.Utilities;
+
+namespace BookCollectionUsingOOps.Managers
+{
+    public class BookManager
+    {
+        private readonly BookRepository _repository;
+
+        public BookManager()
+        {
+            _repository = new BookRepository();
+        }
+        public void ShowMenu()
+        {
+            Console.WriteLine("1. Add Book to the Collection ");
+            Console.WriteLine("2. Update a book in the collection");
+            Console.WriteLine("3. Delete a Book in the collection ");
+            Console.WriteLine("4. Get all the books from the collection");      
+            Console.WriteLine("5. Exit");
+            Console.WriteLine();
+        }
+
+        public void CreateBook()
+        {
+            Book book = new Book();
+
+            int id = AutoIncrement.GenerateId();
+            book.Id = id;
+
+            Console.WriteLine("Enter the year book is published: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            book.Year = year;
+
+            Console.WriteLine("Enter the author of the book: ");
+            string author = Console.ReadLine();
+            book.Author = author;
+
+            Console.WriteLine("Enter the title of the book: ");
+            string title = Console.ReadLine();
+            book.Title = title;
+
+            _repository.CreateBook(book);
+            Console.WriteLine($"Book added successfully to the collection: {book.Title}");
+            Console.WriteLine();
+        }
+
+        public void UpdateBook()
+        {
+            Console.WriteLine("Emter the ID of the book to be updated: ");
+            int idToUpdate = Convert.ToInt32(Console.ReadLine());
+            Book updatedBook = new Book();
+
+            Console.WriteLine("Enter the new year: ");
+            int updatedYear = Convert.ToInt32(Console.ReadLine());
+            updatedBook.Year = updatedYear;
+
+            Console.WriteLine("Enter the new Author: ");
+            string newAuthor = Console.ReadLine();
+            updatedBook.Author = newAuthor;
+
+            Console.WriteLine("Enter the new Title: ");
+            string newTitle = Console.ReadLine();
+            updatedBook.Title = newTitle;
+
+            if(_repository.UpdateBook(idToUpdate, updatedBook)==false)
+                Console.WriteLine("Book cannot be updated!! There is an error in input");
+            else
+            {
+                _repository.UpdateBook(idToUpdate, updatedBook);
+                Console.WriteLine("The book has been updated successfully");
+            }
+            Console.WriteLine();
+            
+        }
+
+        public void ViewBooks()
+        {
+            List<Book> books = _repository.ViewBooks();
+
+            // Print header
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine($"{"ID",-5} {"Title",-30} {"Author",-20} {"Year",-5}");
+            Console.WriteLine("----------------------------------------------------------");
+
+            // Print book details
+            foreach (Book book in books)
+            {
+                Console.WriteLine($"{book.Id,-5} {book.Title,-30} {book.Author,-20} {book.Year,-5}");
+            }
+
+            Console.WriteLine("----------------------------------------------------------");
+
+        }
+
+        public void DeleteBook()
+        {
+            int id = Convert.ToInt32(Console.ReadLine());
+            if (_repository.DeleteBook(id) == false)
+                Console.WriteLine("Book can't be deleted");
+            else
+            {
+                _repository.DeleteBook(id);
+                Console.WriteLine("Book deleted succesfully!!");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BookCollectionUsingOOps/BookCollectionUsingOOps/Program.cs
@@ -0,0 +1,41 @@
+﻿using BookCollectionUsingOOps.Managers;
+
+namespace BookCollectionUsingOOps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookManager manager = new BookManager();
+
+            bool exit = false;
+
+            while(!exit)
+            {
+                Console.WriteLine("Welcome to Book Collection Manager!");
+                manager.ShowMenu();
+                Console.WriteLine("Select your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        manager.CreateBook();
+                        break;
+                    case 2:
+                        manager.UpdateBook();
+                        break;
+                    case 3:
+                        manager.DeleteBook();
+                        break;
+                    case 4:
+                        manager.ViewBooks();
+                        break;
+                    case 5:
+                        Console.WriteLine("Thanks for using our Book Manager!!");
+                        exit = true;
+                        break;
+                }
+            }
+        }
+    }
+}
