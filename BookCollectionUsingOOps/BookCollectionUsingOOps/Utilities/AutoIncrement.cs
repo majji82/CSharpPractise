6d269e69dc1222efbd99285bd67e6134a5b1fb94
majji82 <2100032153cseh@gmail.com>
2025-01-19 22:40:42 +0530

    Done with projects

---
+++ BookCollectionUsingOOps/BookCollectionUsingOOps/Utilities/AutoIncrement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCollectionUsingOOps.Utilities
+{
+    public static class AutoIncrement
+    {
+        private static int _id = 1;
+        public static int GenerateId()
+        {
+            return _id++;
+        }
+    }
+}
